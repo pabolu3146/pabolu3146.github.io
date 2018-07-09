243a2021270d9497a0af70f9101a4174ecd34f34
sravan <pabolu.sravan@gmail.com>
2018-07-09 13:46:13 +0530

    hello

---
+++ web.js
@@ -0,0 +1,14 @@
+function myFunction(){
+alert('hello');
+document.getElementById("myid").innerHTML="you changed it!!"
+document.getElementById("myid2").innerHTML="Oneplus"
+document.getElementById("myid3").innerHTML="oneplusX"
+document.getElementById("myid4").innerHTML="oneplus1"
+document.getElementById("myid5").innerHTML="oneplus2"
+document.getElementById("myid6").innerHTML="oneplus3"
+document.getElementById("myid7").src="http://www.whatmobile.net/wp-content/uploads/2016/07/oneplus-x-1-600x350.jpg?0e0c84"
+document.getElementById("myid8").src="http://www.droidorigin.com/wp-content/uploads/2016/01/Cyanogen-OS-12.1-on-OnePlus-One.jpg"
+document.getElementById("myid9").src="http://www.whatmobile.net/wp-content/uploads/2016/05/OnePlus-2-Style-Swap-Covers-2-600x350.jpg?0e0c84"
+document.getElementById("myid10").src="http://www.whatmobile.net/wp-content/uploads/2016/06/OnePlus3-copy-600x350.jpg"
+document.getElementById("myid12").style.backgroundImage = "url(https://content.oneplus.net/media/wysiwyg/home/home_3_dualcolor_1920.jpg)";
+}
